0c8ebc8a6a67e93f05921edb26583428319328ea
ramzy1453 <kemmounramzy93@gmail.com>
2024-06-21 22:38:07 +0100

    init ws

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,27 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from "@sveltejs/kit/vite";
+import { type ViteDevServer, defineConfig } from "vite";
+import { Server } from "socket.io";
+import type { Item } from "$lib/types";
+
+const webSocketServer = {
+  name: "webSocketServer",
+  configureServer(server: ViteDevServer) {
+    if (!server.httpServer) return;
+
+    const io = new Server(server.httpServer);
+
+    io.on("connection", (socket) => {
+      console.log(`Socket connected: ${socket.id}`);
+      socket.emit("eventFromServer", "Hello, World 👋");
+
+      socket.on("submit-word", (item: Item) => {
+        console.log("submit-word", item);
+        io.emit("word-submitted", item);
+      });
+    });
+  },
+};
 
 export default defineConfig({
-	plugins: [sveltekit()]
+  plugins: [sveltekit(), webSocketServer],
 });
